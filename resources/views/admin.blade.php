cd651c252a6c4d451dc082801d66c1061a501335
johncarlo03 <120325948+johncarlo03@users.noreply.github.com>
2025-05-30 23:28:46 +0800

    admin change

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -154,7 +154,7 @@
                     <thead>
                         <tr class="!font-bold">
                             <th>#</th>
-                            <th>Positionss</th>
+                            <th>Positions</th>
                             <th>Candidate</th>
                             <th>Total Vote</th>
                         </tr>
